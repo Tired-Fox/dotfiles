d6ccc417d3c9ad84e08c6cbf9497cfbd97867e45
TiredFox <zboehm104@gmail.com>
2024-05-19 17:12:29 -0500

    Initial Commit

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.font_size = 10
+config.font = wezterm.font_with_fallback({
+	"MonoLisa Variable",
+	"JetBrainsMono NF",
+})
+
+config.show_new_tab_button_in_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+local padding = 2
+config.window_padding = {
+	left = padding,
+	right = padding,
+	top = padding,
+	bottom = padding,
+}
+
+local flavors = {
+	mocha = "Mocha",
+	macchiato = "Macchiato",
+	frappe = "Frappe",
+	latte = "Latte",
+}
+
+config.color_scheme = "Catppuccin " .. flavors["mocha"]
+
+return config
